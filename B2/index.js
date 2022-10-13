24c9c41a5de12e6fe809d4a7afdee9b7d1c7d43b
dinhnguyenminhhoang <dinhnguyenminhhoang28@gmail.com>
2022-10-13 11:55:39 +0700

    install morgan

--- B2/index.js
+++ B2/index.js
@@ -1,8 +1,11 @@
 //install express
+//install nodemon
+//install morgan
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 const port = 3000;
-
+app.use(morgan("combined"));
 app.get("/tin-tuc", (req, res) => {
   return res.send("hello!");
 });
